17c91533f913ac80e2bba63b636d6d8213a4a44f
Devcossam <cossamsoko7@gmail.com>
2024-09-01 16:04:22 +0200

    modified some html files and css files and worked on the pupils dashboard

--- myproject/pupil/views.py
+++ myproject/pupil/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
-def landing_page(request):
-    template = loader.get_template('pupil/landing_page.html')
+def dashboard(request):
+    template = loader.get_template('pupil/dashboard.html')
     return HttpResponse(template.render())
